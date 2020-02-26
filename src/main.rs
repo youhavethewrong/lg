3bd1ceb755fffffe7e5ca87e26dd759056da5fc2
ESC <youhavethewrong@gmail.com>
2020-02-25 20:35:55 -0600

    Use a toml configuration to drive downloading several items.

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,44 @@
+#![feature(async_closure)]
+use futures::stream::{self, StreamExt};
 use hyper::{Client, Uri};
+use serde_derive::Deserialize;
+use std::fs::File;
+use std::io::Read;
 use tokio::fs;
+
+#[derive(Debug, Deserialize)]
+struct Target {
+    pub url: String,
+    pub filename: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct Targets {
+    pub targets: Vec<Target>,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Main screen turn on.");
 
-    let client = Client::new();
+    let config_path = "config.toml";
+    let mut config_file = File::open(config_path).unwrap();
+    let mut config_buffer = String::new();
+    config_file.read_to_string(&mut config_buffer).unwrap();
+    let decoded: Targets = toml::from_str(&config_buffer).unwrap();
+    let targets = decoded.targets;
+    println!("Fetching {} targets.", targets.len());
+    let mut target_stream = stream::iter(targets);
 
-    let ip_fut = async {
-        let resp = client
-            .get(Uri::from_static("http://httpbin.org/ip"))
-            .await
-            .unwrap();
+    while let Some(target) = target_stream.next().await {
+        let client = Client::new();
+        let parsed_url = target.url.parse::<Uri>().unwrap();
+        let resp = client.get(parsed_url).await.unwrap();
         let body = hyper::body::to_bytes(resp.into_body()).await.unwrap();
-        fs::write("ip.txt", body).await
-    };
-    let headers_fut = async {
-        let resp = client
-            .get(Uri::from_static("http://httpbin.org/headers"))
-            .await
-            .unwrap();
-        let body = hyper::body::to_bytes(resp.into_body()).await.unwrap();
-        fs::write("headers.txt", body).await
-    };
+        fs::write(&target.filename, body).await.unwrap();
+        println!("Wrote {}.", target.filename);
+    }
 
-    let (ip, headers) = futures::try_join!(ip_fut, headers_fut)?;
-    println!("{:?} and {:?}", ip, headers);
+    println!("Done!");
     Ok(())
 }
