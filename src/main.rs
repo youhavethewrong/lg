401e6732cf719a78c907b4db0e538b84a05b58d6
ESC <youhavethewrong@gmail.com>
2020-02-26 21:47:02 -0600

    Talk TLS. Follow 302s.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![feature(async_closure)]
 use futures::stream::{self, StreamExt};
-use hyper::{Client, Uri};
+use hyper::{Client, StatusCode, Uri};
+use hyper_tls::HttpsConnector;
 use serde_derive::Deserialize;
 use std::fs::File;
 use std::io::Read;
@@ -31,12 +32,30 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let target_stream = stream::iter(targets);
 
     let fut = target_stream.for_each_concurrent(4, |target| async move {
-        let client = Client::new();
+        let https = HttpsConnector::new();
+        let client = Client::builder().build::<_, hyper::Body>(https);
         let parsed_url = target.url.parse::<Uri>().unwrap();
         let resp = client.get(parsed_url).await.unwrap();
-        let body = hyper::body::to_bytes(resp.into_body()).await.unwrap();
-        fs::write(&target.filename, body).await.unwrap();
-        println!("Wrote {}.", target.filename);
+        match resp.status() {
+            StatusCode::OK => {
+                let body = hyper::body::to_bytes(resp.into_body()).await.unwrap();
+                fs::write(&target.filename, body).await.unwrap();
+                println!("Wrote {}.", target.filename);
+            }
+            StatusCode::FOUND => {
+                let location = &resp.headers()["Location"];
+                let follow_resp = client
+                    .get(location.to_str().unwrap().parse().unwrap())
+                    .await
+                    .unwrap();
+                let body = hyper::body::to_bytes(follow_resp.into_body())
+                    .await
+                    .unwrap();
+                fs::write(&target.filename, body).await.unwrap();
+                println!("Wrote {}.", target.filename);
+            }
+            _ => println!("Something weird"),
+        }
     });
 
     fut.await;
